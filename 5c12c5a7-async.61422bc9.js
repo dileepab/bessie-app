71efddd426b5c41cf76567f16ce5d646ff16d191
Dileepa <bmdbalasuriya@gmail.com>
2024-11-14 21:55:52 +0530

    Updates

---
+++ 5c12c5a7-async.61422bc9.js
@@ -0,0 +1,2 @@
+(("undefined"!=typeof globalThis?globalThis:self)["makoChunk_ant-design-pro"]=("undefined"!=typeof globalThis?globalThis:self)["makoChunk_ant-design-pro"]||[]).push([["5c12c5a7"],{"5c12c5a7":function(e,t,n){"use strict";n.d(t,"__esModule",{value:!0}),n.d(t,"default",{enumerable:!0,get:function(){return l;}});var u=n("9119baee");n("91a539d5");var a=n("d2d88512");function l(){let e=(0,a.useOutletContext)();return(0,u.jsx)(a.Outlet,{context:e});}}}]);
+//# sourceMappingURL=5c12c5a7-async.61422bc9.js.map
